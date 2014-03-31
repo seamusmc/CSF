213cc5ae47941b1873713f2ed60eac9c809d7667
Seamus McGowan <seamus.mcgowan@mutualmobile.com>
2014-03-31 15:01:39 -0500

    Added custom view for the login scene.

---
+++ CSF/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  CSF
+//
+//  Created by Seamus McGowan on 3/31/14.
+//  Copyright (c) 2014 Seamus McGowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
