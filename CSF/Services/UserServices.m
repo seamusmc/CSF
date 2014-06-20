eaf41b9c2207fa5290ce277e00079448e2bb036c
Seamus McGowan <seamus.mcgowan@mutualmobile.com>
2014-06-20 08:34:43 -0500

    Added Reformatted code

--- CSF/Services/UserServices.m
+++ CSF/Services/UserServices.m
@@ -15,29 +15,22 @@ NSString *const FailedAuthentication = @"FailedAuthentication";
 @interface UserServices ()
 
 // Define this because we can't auto-synthesize protocol properties
-@property (strong, nonatomic) User *currentUser;
-
-@property (strong, nonatomic, readonly) id <NetworkingServiceProtocol> networkingService;
+@property(strong, nonatomic) User *currentUser;
+@property(strong, nonatomic, readonly) id <NetworkingServiceProtocol> networkingService;
 
 @end
 
 @implementation UserServices
-{
 
-}
-
-- (User *)currentUser
-{
-    if (!_currentUser)
-    {
+- (User *)currentUser {
+    if (!_currentUser) {
         [NSException raise:@"Ccurrent User Property Not Set." format:@"The current user property has not been set."];
     }
 
     return _currentUser;
 }
 
-- (void)storeUser:(User *)user withPassword:(NSString *)password
-{
+- (void)storeUser:(User *)user withPassword:(NSString *)password {
     FXKeychain *keychain = [FXKeychain defaultKeychain];
 
     keychain[@"firstname"] = user.firstname;
@@ -47,8 +40,7 @@ NSString *const FailedAuthentication = @"FailedAuthentication";
     keychain[@"password"]  = password;
 }
 
-- (void)retrieveUserAndPasswordFromStoreWithCompletionHandler:(void (^)(User *user, NSString *password))completionHandler
-{
+- (void)retrieveUserAndPasswordFromStoreWithCompletionHandler:(void (^)(User *user, NSString *password))completionHandler {
     FXKeychain *keychain = [FXKeychain defaultKeychain];
 
     User *usr = [[User alloc] initWithFirstname:keychain[@"firstname"]
@@ -61,61 +53,54 @@ NSString *const FailedAuthentication = @"FailedAuthentication";
     completionHandler(usr, password);
 }
 
-- (void)authenticateUser:(User *)user withPassword:(NSString *)password withCompletionHandler:(void (^)(BOOL))completionHandler
-{
-    if (!completionHandler)
-    {
+- (void)authenticateUser:(User *)user
+            withPassword:(NSString *)password
+   withCompletionHandler:(void (^)(BOOL))completionHandler {
+    if (!completionHandler) {
         return;
     }
 
     NSString *uri = [NSString stringWithFormat:AuthenticationURI, user.farm, user.firstname, user.lastname, password];
     [self.networkingService getDataWithURI:uri withCompletionHandler:^(id responseObject)
-    {
-        if (responseObject)
-        {
-            NSLog(@"Order JSON: %@", responseObject);
-
-            // A code of 2 indicates authentication failed. Could be because firstname, lastname,
-            // password or farm were not set correctly.
-            NSDictionary *errorInfo = [responseObject objectForKey:@"ErrorInfo"];
-
-            NSInteger code = [((NSString *) [errorInfo objectForKey:@"Code"]) integerValue];
-            if (code == 2)
             {
-                dispatch_async(dispatch_get_main_queue(), ^
-                                                          {
-                                                              [[NSNotificationCenter defaultCenter] postNotificationName:FailedAuthentication object:self];
-                                                          });
-                completionHandler(NO);
-            }
-            else
-            {
-                self.currentUser = [[User alloc] initWithFirstname:user.firstname
-                                                          lastname:user.lastname
-                                                             group:[responseObject objectForKey:@"Group"]
-                                                              farm:user.farm];
-                completionHandler(YES);
-            }
-        }
-        else
-        {
-            completionHandler(NO);
-        }
-    }];
+                if (responseObject) {
+                    NSLog(@"Order JSON: %@", responseObject);
+
+                    // A code of 2 indicates authentication failed. Could be because firstname, lastname,
+                    // password or farm were not set correctly.
+                    NSDictionary *errorInfo = [responseObject objectForKey:@"ErrorInfo"];
+
+                    NSInteger code = [((NSString *) [errorInfo objectForKey:@"Code"]) integerValue];
+                    if (code == 2) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            [[NSNotificationCenter defaultCenter] postNotificationName:FailedAuthentication
+                                                                                object:self];
+                        });
+                        completionHandler(NO);
+                    }
+                    else {
+                        self.currentUser = [[User alloc] initWithFirstname:user.firstname
+                                                                  lastname:user.lastname
+                                                                     group:[responseObject objectForKey:@"Group"]
+                                                                      farm:user.farm];
+                        completionHandler(YES);
+                    }
+                }
+                else {
+                    completionHandler(NO);
+                }
+            }];
 }
 
-- (id <NetworkingServiceProtocol>)networkingService
-{
+- (id <NetworkingServiceProtocol>)networkingService {
     return [ServiceLocator sharedInstance].networkingService;
 }
 
-+ (id <UserServicesProtocol>)sharedInstance
-{
++ (id <UserServicesProtocol>)sharedInstance {
     static UserServices    *sharedInstance = nil;
     static dispatch_once_t onceToken;
 
-    dispatch_once(&onceToken, ^
-    {
+    dispatch_once(&onceToken, ^{
         sharedInstance = [[UserServices alloc] init];
     });
 
