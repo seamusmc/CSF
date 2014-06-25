1c0fe5a9e5920b49e0edd4ff83b42d58ed0316e6
Seamus McGowan <seamus.mcgowan@mutualmobile.com>
2014-06-25 11:14:54 -0500

    Progress toward more robust error handling

--- CSF/Services/NetworkingService.m
+++ CSF/Services/NetworkingService.m
@@ -13,34 +13,47 @@
     uri = [uri stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSURL *url = [NSURL URLWithString:uri];
 
-    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
-    [sessionConfig setHTTPAdditionalHeaders:@{@"Accept" : @"application/json"}];
-    sessionConfig.timeoutIntervalForRequest = 10.0;
-    sessionConfig.timeoutIntervalForResource = 10.0;
-
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    NSURLSession *session = [self createSession];
 
     NSURLSessionDataTask *task = [session dataTaskWithURL:url
                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
                 [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
 
                 NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
-                if (httpResponse.statusCode == 200 && !error) {
-                    id responseObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                    completionHandler(responseObject);
-                }
-                else {
-                    DDLogWarn(@"WARN: %s Bad Status: %ld.", __PRETTY_FUNCTION__, (long) httpResponse.statusCode);
-
-                    if (error) {
-                        DDLogError(@"ERROR: %s Message: %@.", __PRETTY_FUNCTION__, error);
+                switch (httpResponse.statusCode) {
+                    case NetworkingServiceCodeSuccess: {
+                        id responseObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                        completionHandler(responseObject);
+                        break;
+                    }
+                    case NetworkingServiceCodeBadRequest: {
+                        NSString *description = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                        DDLogWarn(@"WARN: %s Bad Status: %ld Message: %@", __PRETTY_FUNCTION__, (long) httpResponse.statusCode, description);
+                        break;
                     }
 
-                    completionHandler(NULL);
+                    default: {
+                        DDLogWarn(@"WARN: %s Bad Status: %ld.", __PRETTY_FUNCTION__, (long) httpResponse.statusCode);
+                        if (error) {
+                            DDLogError(@"ERROR: %s Message: %@.", __PRETTY_FUNCTION__, error);
+                        }
+                        completionHandler(NULL);
+                        break;
+                    }
                 }
             }];
 
     [task resume];
+}
+
+- (NSURLSession *)createSession {
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    [sessionConfig setHTTPAdditionalHeaders:@{@"Accept" : @"application/json"}];
+    sessionConfig.timeoutIntervalForRequest  = 10.0;
+    sessionConfig.timeoutIntervalForResource = 10.0;
+
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    return session;
 }
 
 + (id <NetworkingServiceProtocol>)sharedInstance {
