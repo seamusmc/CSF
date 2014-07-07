255e0a485e6b021d70ef60abe26053bc20e9fcc4
Seamus McGowan <seamus.mcgowan@mutualmobile.com>
2014-07-07 13:48:14 -0500

    Adding a couple transition animations

---
+++ CSF/Animation Controllers/CLSlideToRightAnimationController.h
@@ -0,0 +1,10 @@
+//
+// Created by Seamus McGowan on 5/19/14.
+// Copyright (c) 2014 Clover. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLSlideToRightAnimationController : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
---
+++ CSF/Animation Controllers/CLSlideToRightAnimationController.m
@@ -0,0 +1,44 @@
+//
+// Created by Seamus McGowan on 5/19/14.
+// Copyright (c) 2014 Clover. All rights reserved.
+//
+
+#import "CLSlideToRightAnimationController.h"
+//#import "CLTweaksService.h"
+
+@implementation CLSlideToRightAnimationController
+
+- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext {
+    //return [CLTweaksService sharedInstance].slideRightAnimationDuration;
+    return 0.5;
+}
+
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
+    UIViewController *toViewController   = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+
+    CGRect frame = toViewController.view.frame;
+    frame.origin.x -= frame.size.width;
+    toViewController.view.frame = frame;
+
+    UIView *containerView = [transitionContext containerView];
+    [containerView addSubview:toViewController.view];
+
+    NSTimeInterval duration = [self transitionDuration:transitionContext];
+    [UIView animateWithDuration:duration animations:^{
+
+        CGRect frame = fromViewController.view.frame;
+        frame.origin.x += frame.size.width;
+        fromViewController.view.frame = frame;
+
+        frame = toViewController.view.frame;
+        frame.origin.x += frame.size.width;
+        toViewController.view.frame = frame;
+
+    } completion:^(BOOL finished) {
+        [fromViewController.view removeFromSuperview];
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+@end
