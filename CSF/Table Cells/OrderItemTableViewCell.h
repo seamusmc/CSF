006291235329218bd960a8d76795b14a91514d96
Seamus McGowan <seamus.mcgowan@gmail.com>
2014-07-24 10:42:52 -0500

    Adding custom table view cell for order items.

---
+++ CSF/Table Cells/OrderItemTableViewCell.h
@@ -0,0 +1,10 @@
+//
+// Created by Seamus McGowan on 7/24/14.
+// Copyright (c) 2014 Seamus McGowan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface OrderItemTableViewCell : UITableViewCell
+@end
---
+++ CSF/Table Cells/OrderItemTableViewCell.m
@@ -0,0 +1,11 @@
+//
+// Created by Seamus McGowan on 7/24/14.
+// Copyright (c) 2014 Seamus McGowan. All rights reserved.
+//
+
+#import "OrderItemTableViewCell.h"
+
+
+@implementation OrderItemTableViewCell
+
+@end
