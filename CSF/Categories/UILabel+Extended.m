c2c4d997df8717b9b03a9455b6593af2964ecdce
Seamus McGowan <seamus.mcgowan@gmail.com>
2014-08-05 09:25:35 -0500

    Added some more swizzling of UILabel for the date picker to control the font and case of the strings in the picker.

--- CSF/Categories/UILabel+Extended.m
+++ CSF/Categories/UILabel+Extended.m
@@ -5,25 +5,54 @@
 
 #import <objc/runtime.h>
 #import "UILabel+Extended.h"
+#import "ThemeManager.h"
 
 @implementation UILabel (Extended)
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self swizzleInstanceSelector:@selector(setTextColor:)
-                      withNewSelector:@selector(swizzledSetTextColor:)];
-        [self swizzleInstanceSelector:@selector(willMoveToSuperview::)
-                      withNewSelector:@selector(swizzledWillMoveToSuperview:)];
+        [self swizzleInstanceSelector:@selector(setTextColor:) withNewSelector:@selector(swizzledSetTextColor:)];
+        [self swizzleInstanceSelector:@selector(willMoveToSuperview::) withNewSelector:@selector(swizzledWillMoveToSuperview:)];
+        [self swizzleInstanceSelector:@selector(setFont:) withNewSelector:@selector(swizzledSetFont:)];
+        [self swizzleInstanceSelector:@selector(setText:) withNewSelector:@selector(swizzledSetText:)];
+
     });
 }
 
+- (void)swizzledSetText:(NSString *)text {
+    if ([self view:self hasSuperviewOfClass:[UIDatePicker class]] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerWeekMonthDayView")] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerContentView")]) {
+
+        [self swizzledSetText:[text lowercaseString]];
+    } else {
+        //Carry on with the default
+        [self swizzledSetText:text];
+    }
+}
+
+- (void)swizzledSetFont:(UIFont *)font {
+    if ([self view:self hasSuperviewOfClass:[UIDatePicker class]] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerWeekMonthDayView")] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerContentView")]) {
+
+        [self swizzledSetFont:[ThemeManager sharedInstance].normalFont];
+    } else {
+        //Carry on with the default
+        [self swizzledSetFont:font];
+    }
+}
+
 // Forces the text colour of the label to be white only for UIDatePicker and its components
--(void) swizzledSetTextColor:(UIColor *)textColor {
-    if([self view:self hasSuperviewOfClass:[UIDatePicker class]] ||
-       [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerWeekMonthDayView")] ||
-       [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerContentView")]){
-        [self swizzledSetTextColor:[UIColor whiteColor]];
+- (void)swizzledSetTextColor:(UIColor *)textColor {
+    if ([self view:self hasSuperviewOfClass:[UIDatePicker class]] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerWeekMonthDayView")] ||
+        [self view:self hasSuperviewOfClass:NSClassFromString(@"UIDatePickerContentView")]) {
+
+//        label.font          = [ThemeManager sharedInstance].normalFont;
+
+        [self swizzledSetTextColor:[ThemeManager sharedInstance].normalFontColor];
     } else {
         //Carry on with the default
         [self swizzledSetTextColor:textColor];
@@ -31,15 +60,15 @@
 }
 
 // Some of the UILabels haven't been added to a superview yet so listen for when they do.
-- (void) swizzledWillMoveToSuperview:(UIView *)newSuperview {
+- (void)swizzledWillMoveToSuperview:(UIView *)newSuperview {
     [self swizzledSetTextColor:self.textColor];
     [self swizzledWillMoveToSuperview:newSuperview];
 }
 
 // -- helpers --
-- (BOOL) view:(UIView *) view hasSuperviewOfClass:(Class) class {
-    if(view.superview){
-        if ([view.superview isKindOfClass:class]){
+- (BOOL)view:(UIView *)view hasSuperviewOfClass:(Class)class {
+    if (view.superview) {
+        if ([view.superview isKindOfClass:class]) {
             return true;
         }
         return [self view:view.superview hasSuperviewOfClass:class];
@@ -47,11 +76,10 @@
     return false;
 }
 
-+ (void) swizzleInstanceSelector:(SEL)originalSelector
-                 withNewSelector:(SEL)newSelector
-{
++ (void)swizzleInstanceSelector:(SEL)originalSelector
+                withNewSelector:(SEL)newSelector {
     Method originalMethod = class_getInstanceMethod(self, originalSelector);
-    Method newMethod = class_getInstanceMethod(self, newSelector);
+    Method newMethod      = class_getInstanceMethod(self, newSelector);
 
     BOOL methodAdded = class_addMethod([self class],
                                        originalSelector,
