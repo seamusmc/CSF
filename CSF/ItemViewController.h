46f113677d8a63577a93c33766d9f76039269dc8
Seamus McGowan <seamus.mcgowan@gmail.com>
2014-08-15 07:21:49 -0500

    Adding view controller for the item page.

---
+++ CSF/ItemViewController.h
@@ -0,0 +1,14 @@
+//
+//  ItemViewController.h
+//  CSF
+//
+//  Created by Seamus McGowan on 8/15/14.
+//  Copyright (c) 2014 Seamus McGowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface ItemViewController : BaseViewController
+
+@end
---
+++ CSF/ItemViewController.m
@@ -0,0 +1,37 @@
+//
+//  ItemViewController.m
+//  CSF
+//
+//  Created by Seamus McGowan on 8/15/14.
+//  Copyright (c) 2014 Seamus McGowan. All rights reserved.
+//
+
+#import "ItemViewController.h"
+
+@interface ItemViewController ()
+
+@end
+
+@implementation ItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
