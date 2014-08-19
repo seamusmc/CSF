fcd0f9baaad6c8fa55b16a9e6287953a61c99a43
Seamus McGowan <seamus.mcgowan@gmail.com>
2014-08-19 08:28:51 -0500

    Started to layout and configure the item page.

--- CSF/ItemViewController.m
+++ CSF/ItemViewController.m
@@ -7,8 +7,27 @@
 //
 
 #import "ItemViewController.h"
+#import "ThemeManager.h"
 
 @interface ItemViewController ()
+
+@property(nonatomic, strong) NSArray *labels;
+@property(nonatomic, strong) NSArray *textFields;
+
+@property(nonatomic, weak) IBOutlet UILabel *typeLabel;
+@property(nonatomic, weak) IBOutlet UILabel *itemLabel;
+@property(nonatomic, weak) IBOutlet UILabel *priceLabel;
+@property(nonatomic, weak) IBOutlet UILabel *stockLabel;
+@property(nonatomic, weak) IBOutlet UILabel *quantityLabel;
+@property(nonatomic, weak) IBOutlet UILabel *commentLabel;
+
+@property(nonatomic, weak) IBOutlet UITextField *typeTextField;
+@property(nonatomic, weak) IBOutlet UITextField *itemTextField;
+@property(nonatomic, weak) IBOutlet UITextField *quantityTextField;
+
+@property(nonatomic, weak) IBOutlet UITextView *commentTextView;
+
+@property(nonatomic, weak) IBOutlet UIButton *addButton;
 
 @end
 
@@ -18,6 +37,7 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor clearColor];
+    [self configureLabels];
 }
 
 /*
@@ -29,5 +49,13 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)configureLabels {
+    self.labels = @[self.typeLabel, self.itemLabel, self.priceLabel, self.stockLabel, self.quantityLabel, self.commentLabel];
+    for (UILabel *label in self.labels) {
+        label.font = [ThemeManager sharedInstance].normalFont;
+        label.textColor = [ThemeManager sharedInstance].normalFontColor;
+    }
+}
 
 @end
